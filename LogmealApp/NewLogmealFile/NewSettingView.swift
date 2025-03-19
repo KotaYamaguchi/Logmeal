33ccb169ec76f616663892c83718a3f78e92bde8
KotaYamaguchi <k-yamaguchi@ilab.pu-kumamoto.ac.jp>
2025-03-19 17:43:07 +0900

    音量設定とチュートリアル閲覧のViewを追加。画面は遷移しない

--- LogmealApp/NewLogmealFile/NewSettingView.swift
+++ LogmealApp/NewLogmealFile/NewSettingView.swift
@@ -3,6 +3,10 @@ import SwiftUI
 
 struct NewSettingView: View {
     private let tutorialImage: [String] = ["HowToUseHome", "HowToUseCalendar", "HowToUseShop", "HowToUseCharacter", "HowToUseColumnList", "HowToUseAjiwaiCard", "HowToUseQr", "HowToUseCardEdit", "HowToUseSetting", "HowToUseShare1", "HowToUseShare2"]
+    @ObservedObject private var soundManager = SoundManager.shared
+    @ObservedObject private var bgmManager = BGMManager.shared
+    @State private var bgmVolume: Float = BGMManager.shared.bgmVolume
+    @EnvironmentObject var userData: UserData
     var body: some View {
         NavigationStack {
             ZStack{
@@ -18,7 +22,7 @@ struct NewSettingView: View {
                         .resizable()
                         .scaledToFit()
                         .frame(width:550)
-                    settingRow(destination: NewProfileEditView(), imageName: "mt_newSettingView_profile")
+                    settingRow(destination: NewProfileEditView(userData: userData), imageName: "mt_newSettingView_profile")
                     settingRow(destination: soundSettingView(), imageName: "mt_newSettingView_sound")
                     settingRow(destination: dataShareView(), imageName: "mt_newSettingView_share")
                     settingRow(destination: otherSettingView(), imageName: "mt_newSettingView_others")
@@ -46,7 +50,113 @@ struct NewSettingView: View {
     }
     private func soundSettingView() -> some View{
         VStack{
-            
+            VStack {
+                Text("BGMの音量")
+                    .font(.custom("GenJyuuGothicX-Bold", size: 15))
+                HStack {
+                    Button {
+                        bgmVolume = max(bgmVolume - 0.1, 0)
+                        bgmManager.setBGMVolume(bgmVolume)
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.title)
+                            .foregroundStyle(.orange)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Slider(value: $bgmVolume, in: 0...1, onEditingChanged: { editing in
+                        if !editing {
+                            bgmManager.setBGMVolume(bgmVolume)
+                        }
+                    })
+                    .tint(.orange)
+                    .padding(.horizontal)
+                    Button {
+                        bgmVolume = min(bgmVolume + 0.1, 1)
+                        bgmManager.setBGMVolume(bgmVolume)
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .foregroundStyle(.orange)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Button {
+                        bgmManager.toggleBGM()
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 65, height: 35)
+                                .foregroundStyle(Color(red: 0.42, green: 0.4, blue: 0.4))
+                                .offset(y: 5)
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 65, height: 35)
+                                .foregroundStyle(bgmManager.isBGMOn ? .orange : .gray)
+                                .overlay {
+                                    Text(bgmManager.isBGMOn ? "ON" : "OFF")
+                                        .font(.title)
+                                        .foregroundStyle(.white)
+                                }
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .padding()
+            // SE音量調整スライダー
+            VStack {
+                Text("効果音の音量")
+                    .font(.custom("GenJyuuGothicX-Bold", size: 15))
+                HStack {
+                    Button {
+                        soundManager.soundVolume = max(soundManager.soundVolume - 0.1, 0)
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.title)
+                            .foregroundStyle(.orange)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Slider(value: Binding(
+                        get: { soundManager.soundVolume },
+                        set: { newVolume in
+                            soundManager.setSoundVolume(newVolume)
+                        }
+                    ), in: 0...1, onEditingChanged: { editing in
+                        if !editing {
+                            soundManager.setSoundVolume(soundManager.soundVolume)
+                        }
+                    })
+                    .tint(.orange)
+                    .padding(.horizontal)
+                    Button {
+                        soundManager.soundVolume = min(soundManager.soundVolume + 0.1, 1)
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .foregroundStyle(.orange)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    Button{
+                        soundManager.toggleSound()
+                        
+                    }label:{
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 65, height: 35)
+                                .foregroundStyle(Color(red: 0.42, green: 0.4, blue: 0.4))
+                                .offset(y:5)
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 65, height: 35)
+                                .foregroundStyle( soundManager.isSoundOn ? .orange : .gray)
+                                .overlay{
+                                    Text( soundManager.isSoundOn ? "ON" : "OFF")
+                                        .font(.title)
+                                        .foregroundStyle(.white)
+                                }
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+            .padding()
         }
     }
     private func dataShareView() -> some View{
@@ -66,17 +176,18 @@ struct NewSettingView: View {
     }
     private func howUseAppView() -> some View{
         VStack{
-            
+            TutorialView(imageArray: tutorialImage)
         }
     }
 }
 
 struct NewProfileEditView: View {
+    @ObservedObject var userData:UserData
+    
     @State private var userName: String = ""
     @State private var userGrade: Int = 1
     @State private var userClass: Int = 1
     @State private var userAge: Int = 6
-    
     var body: some View {
         ZStack {
             Image("bg_newSettingView.png")
@@ -181,3 +292,6 @@ struct NewProfileEditView: View {
 }
 
 
+#Preview{
+    NewContentView()
+}
